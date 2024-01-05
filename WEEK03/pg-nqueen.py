71b33acec5a93f23e483c7df381518594b742a03
최현서 <117807082+ha01032@users.noreply.github.com>
2024-01-05 11:21:07 +0900

    Create pg-nqueen.py

---
+++ WEEK03/pg-nqueen.py
@@ -0,0 +1,27 @@
+answer=0
+arrX=[]
+arrY=[]
+arrPlus = []
+arrMinus = []
+
+def solution(n):
+    def makeQ(x):
+        global answer, arrX, arrY, arrPlus, arrMinus
+        if len(arrX) == n: 
+            answer+=1
+            return
+
+        for y in range(n):
+                if(not x in arrX and not y in arrY and not (x+y) in arrPlus and not (x-y) in arrMinus):
+
+                    arrX.append(x)
+                    arrY.append(y)
+                    arrPlus.append(x+y)
+                    arrMinus.append(x-y)
+                    makeQ(x+1)
+                    arrX.pop()
+                    arrY.pop()
+                    arrPlus.pop()
+                    arrMinus.pop()
+    makeQ(1)
+    return answer
